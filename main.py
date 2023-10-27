c5dbfd4f0a4fa26be87466252467f94b40350e55
Nurbek <nurbek.malikov@nu.edu.kz>
2023-10-27 21:32:21 +0600

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class HelloWorld():
+    def read_hello(self):
+        return {"data": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+router = APIRouter()
+router.add_api_route('/api/v2/hello-world',
+                     endpoint=HelloWorld().read_hello, methods=["GET"])
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
