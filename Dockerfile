8fef510b4c0350b93840787b47d3cce01e062bb6
Nurbek <nurbek.malikov@nu.edu.kz>
2023-11-14 05:39:15 +0600

    Added navbar component

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.11-slim
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 WORKDIR /app/
-RUN pip install --upgrade pip
 COPY . .
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
+
 EXPOSE 8000
 CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- static/src/App.tsx
+++ static/src/App.tsx
@@ -1,18 +1,11 @@
-import Navbar from "./components/Navbar.tsx";
-import Main from "./components/Main.tsx";
-import PredictionCar from "./components/PredictionCar.tsx";
-import PredictionYolo from "./components/PredictionYolo.tsx";
+import HomePage from "./pages/HomePage.tsx";
+
 
 function App() {
 
 
     return (
-        <>
-            <Navbar></Navbar>
-            <Main></Main>
-            <PredictionCar></PredictionCar>
-            <PredictionYolo></PredictionYolo>
-        </>
+        <HomePage />
     );
 }
 
